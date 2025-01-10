d068c6475c41167f6fb5ca03db25135a9966eb8d
cork03 <64002008+cork03@users.noreply.github.com>
2025-01-10 11:55:05 +0900

    フロントから生産者を作る

--- front/src/components/wine/new/CreateProducers.tsx
+++ front/src/components/wine/new/CreateProducers.tsx
@@ -1,10 +1,33 @@
-import {Producer} from "@/app/wine/new/page";
+"use client"
+
+import React, {useState} from "react";
 
 const CreateProducers = () => {
+    const [producerData, setProducerData] = useState({name: ''});
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setProducerData({...producerData, [e.target.name]: e.target.value});
+    }
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/producer`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(producerData)
+            })
+            if (!response.ok) {
+                throw new Error('Failed to create producer');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
         <section className="border-t pt-8">
             <h3 className="text-xl font-bold text-center mb-6">新しい生産者を作成</h3>
-            <form className="mx-auto space-y-4">
+            <form className="mx-auto space-y-4" onSubmit={handleSubmit}>
                 {/* 名前 */}
                 <div>
                     <label className="block text-sm font-medium text-gray-700 mb-1">
@@ -12,6 +35,9 @@ const CreateProducers = () => {
                     </label>
                     <input
                         type="text"
+                        name="name"
+                        value={producerData.name}
+                        onChange={handleChange}
                         placeholder="生産者名を入力"
                         className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-gray-400"
                     />
