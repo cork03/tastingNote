734cb5b28c7ed6da866fb4a259a345e0fbf8afb6
cork03 <64002008+cork03@users.noreply.github.com>
2025-01-29 01:39:18 +0900

    コメントの表示

--- front/src/components/wine/[id]/vintage/[vintage]/WineComment.tsx
+++ front/src/components/wine/[id]/vintage/[vintage]/WineComment.tsx
@@ -18,46 +18,47 @@ const WineCommentPage = ({id, vintage, wineComments}: Props) => {
                     テイスティングコメント
                 </h2>
             </div>
-            <div className="border rounded-lg shadow-lg p-6 bg-gray-100">
+            <div className="space-y-6">
                 {wineComments.map((wineComment) => {
                     return (
-                        <div className="space-y-6">
-                            <div className="flex flex-col">
+                        <div
+                            key={wineComment.id}
+                            className="border rounded-lg shadow-lg p-6 hover:shadow-xl transition-shadow duration-300 bg-gray-100"
+                        >
+                            <div className="flex flex-col mb-4">
                                 <label className="text-lg font-medium text-gray-800 mb-2">色調・外観</label>
                                 <p className="text-lg text-gray-700 font-semibold">{wineComment.appearance}</p>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col mb-4">
                                 <label className="text-lg font-medium text-gray-800 mb-2">香り</label>
                                 <p className="text-lg text-gray-700 font-semibold">{wineComment.aroma}</p>
                             </div>
-                            <div className="flex flex-col">
+                            <div className="flex flex-col mb-4">
                                 <label className="text-lg font-medium text-gray-800 mb-2">味わい</label>
                                 <p className="text-lg text-gray-700 font-semibold">{wineComment.taste}</p>
                             </div>
-                            {
-                                wineComment.anotherComment && (
-                                    <div className="flex flex-col">
-                                        <label className="text-lg font-medium text-gray-800 mb-2">その他コメント</label>
-                                        <p className="text-lg text-gray-700 font-semibold">{wineComment.anotherComment}</p>
-                                    </div>
-                                )
-                            }
+                            {wineComment.anotherComment && (
+                                <div className="flex flex-col">
+                                    <label className="text-lg font-medium text-gray-800 mb-2">その他コメント</label>
+                                    <p className="text-lg text-gray-700 font-semibold">{wineComment.anotherComment}</p>
+                                </div>
+                            )}
                         </div>
-                    )
+                    );
                 })}
             </div>
             <div className="flex flex-row justify-center items-center gap-x-10 mx-auto">
                 <Link href={`/wine/${id}/vintage/${vintage}/create_comment`}>
                     <button
                         type="button"
-                        className="bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-400"
+                        className="bg-gray-700 text-white py-2 px-4 rounded-lg hover:bg-gray-900 focus:outline-none focus:ring focus:ring-gray-400 transition-all duration-300"
                     >
                         コメントを追加する
                     </button>
                 </Link>
-
             </div>
         </div>
+
     )
 }
 
